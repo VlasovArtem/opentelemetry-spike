098c326f6a889d8a115ede2abfbdcdebdf1ad6f4
Artem Vlasov <vlasovartem21@gmail.com>
2023-03-24 13:11:09 +0200

    Fix

--- pkg/opentelemetry/exporter.go
+++ pkg/opentelemetry/exporter.go
@@ -5,12 +5,12 @@ import (
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
-	sdk_trace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/sdk/trace"
 	"google.golang.org/grpc/credentials"
 	"io"
 )
 
-func NewWriterExporter(w io.Writer) (sdk_trace.SpanExporter, error) {
+func NewWriterExporter(w io.Writer) (trace.SpanExporter, error) {
 	return stdouttrace.New(
 		stdouttrace.WithWriter(w),
 		// Use human-readable output.
@@ -20,7 +20,7 @@ func NewWriterExporter(w io.Writer) (sdk_trace.SpanExporter, error) {
 	)
 }
 
-func NewGrpcExporter(collectorURL string, insecure bool) (sdk_trace.SpanExporter, error) {
+func NewGrpcExporter(collectorURL string, insecure bool) (trace.SpanExporter, error) {
 	secureOption := otlptracegrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, ""))
 	if insecure {
 		secureOption = otlptracegrpc.WithInsecure()
